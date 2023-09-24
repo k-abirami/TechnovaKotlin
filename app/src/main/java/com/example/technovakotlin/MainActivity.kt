e4f69cca669a5bbd50571121f4b82aa77eae96b5
Lucy Wu <89555431+LucyWuu@users.noreply.github.com>
2023-09-24 09:20:16 -0400

    Modified main to include sms texting

--- app/src/main/java/com/example/technovakotlin/MainActivity.kt
+++ app/src/main/java/com/example/technovakotlin/MainActivity.kt
@@ -1,32 +1,63 @@
-package com.example.technovakotlin
+package com.example.myapplication
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.telephony.SmsManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.technovakotlin.databinding.ActivityMainBinding
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var phoneEdt: EditText
+    lateinit var messageEdt: EditText
+    lateinit var sendMsgBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        phoneEdt = findViewById(R.id.idEdtPhone)
+        messageEdt = findViewById(R.id.idEdtMessage)
+        sendMsgBtn = findViewById(R.id.idBtnSendMessage)
 
-        val navView: BottomNavigationView = binding.navView
+        // Set the OnClickListener for the sendMsgBtn.
+        sendMsgBtn.setOnClickListener {
+            // Check for SMS permission and request if needed.
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), 1)
+            } else {
+                // Permission already granted, proceed to send SMS.
+                sendSMS()
+            }
+        }
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    private fun sendSMS() {
+        val phoneNumber = phoneEdt.text.toString()
+        val message = messageEdt.text.toString()
+
+        try {
+            val smsManager: SmsManager
+            if (Build.VERSION.SDK_INT >= 23) {
+                smsManager = SmsManager.getDefault()
+            } else {
+                smsManager = SmsManager.getDefault()
+            }
+
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+
+            // Display a toast message for message sent.
+            Toast.makeText(applicationContext, "Message Sent", Toast.LENGTH_LONG).show()
+
+        } catch (e: Exception) {
+            // Display a toast message for error.
+            Toast.makeText(applicationContext, "Please enter all the data.."+e.message.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
